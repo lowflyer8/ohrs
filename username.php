0e62599ace7e262f127a8d2cf8a00278a9dab76e
lowflyer8 <70099263+lowflyer8@users.noreply.github.com>
2025-10-03 19:53:37 +0530

    Add files via upload
    the following files allow user to register and send data to database

---
+++ username.php
@@ -0,0 +1,15 @@
+<?php
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+
+    $conn = new mysqli('localhost','root','','test');
+    if($conn->connect_error){
+        die('connection failed' .$connect_error);
+    }else{
+        $stmt = $conn->prepare("insert into users(username,password)values(?,?)");
+        $stmt->bind_param("ss",$username,$password);
+        echo"registration successfully";
+        $stmt->close();
+        $conn->close();
+    }
+?>
